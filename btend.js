83b9302d989121d5905b0e025b29a559824cc1dc
Sidney Wagner <herrwagner1680@gmail.com>
2016-10-14 12:16:00 -0400

    first upload for LMB static page site

---
+++ btend.js
@@ -0,0 +1,5 @@
+/* SEE http://www.w3schools.com/howto/howto_js_sidenav.asp
+for Side To NAVIGATION */
+
+/* Carousel examples: http://codepen.io/balix/pen/fiBFx
+and http://owlgraphic.com/owlcarousel/demos/itemsCustom.html */
